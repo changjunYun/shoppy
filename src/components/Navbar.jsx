6922cc368320c03386dd4b0482ee51596e72b8be
changjun <yuncj5925@gmail.com>
2024-09-20 23:47:58 +0900

    로그인 사용자 나타내기 (#1)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,15 @@ import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
 import { login, logout, onUserStateChange } from '../api/firebase';
+import User from './User';
 
 export default function Navbar() {
-    const [user, setUser] = useState();
+  const [user, setUser] = useState();
 
-    useEffect(() => {
-        // 생략가능
-        onUserStateChange(setUser);
+  useEffect(() => {
+    onUserStateChange(setUser);
+  }, []);
 
-        // onUserStateChange((user) => {
-        //     console.log(user);
-        //     setUser(user);
-        // });
-    }, [])
-   
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
       <Link to='/' className='flex items-center text-4xl text-brand'>
@@ -29,8 +24,9 @@ export default function Navbar() {
         <Link to='/products/new' className='text-2xl'>
           <BsFillPencilFill />
         </Link>
-       {!user && <button onClick={login}>Login</button>}
-       {user && <button onClick={logout}>Logout</button>}
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
       </nav>
     </header>
   );
