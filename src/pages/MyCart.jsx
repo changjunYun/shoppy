d4d3297fb5d825fe27e60e00a156cb4941a1880f
changjun <yuncj5925@gmail.com>
2024-09-20 11:19:07 +0900

    라우터 구현 (#1)

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function MyCart() {
     return (
         <div>
-            
+            MyCart
         </div>
     );
 }
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ProductDetail() {
     return (
         <div>
-            
+            ProductDetail
         </div>
     );
 }
