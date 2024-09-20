1261333b817c8e5785aee1c017c921f6ac2f6936
changjun <yuncj5925@gmail.com>
2024-09-21 00:46:52 +0900

    admin 판별하기 구현 완료 (#1)

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,6 +6,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
+import { getDatabase, ref, child, get } from 'firebase/database';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -19,6 +20,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
+const database = getDatabase(app);
 
 export function login() {
   signInWithPopup(auth, provider).catch(console.error);
@@ -28,8 +30,21 @@ export function logout() {
     signOut(auth).catch(console.error);
 }
 
-export function onUserStateChange(callback){
-    onAuthStateChanged(auth, (user) => {
-        callback(user);
+export function onUserStateChange(callback) {
+    onAuthStateChanged(auth, async (user) => {
+      const updatedUser = user ? await adminUser(user) : null;
+      callback(updatedUser);
     });
-}
+  }
+  
+  async function adminUser(user) {
+    return get(ref(database, 'admins')) //
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const admins = snapshot.val();
+          const isAdmin = admins.includes(user.uid);
+          return { ...user, isAdmin };
+        }
+        return user;
+      });
+  }
